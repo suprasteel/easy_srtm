cd6ddacc2acb6db100f735b353f950f9a56fb696
Aurélien R <aurelien.rastel@gmail.com>
2022-03-25 00:50:25 +0100

    fast reader

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,132 @@
+use anyhow::Result;
 use byteorder;
+use dotenv::Result;
 use std::fs;
+use thiserror::Error;
+///
+/// # SRTM files reader.
+///
+/// This library provides simple function to access SRTM files elevation.
+/// Its main purpose is the get the elevation directly from latitude and longitude values.
+///
+/// > - See SRTM [description](https://www.usgs.gov/centers/eros/science/usgs-eros-archive-digital-elevation-shuttle-radar-topography-mission-srtm-non)
+/// > - See the [tiles downloader](http://dwtkns.com/srtm30m/)
+/// > - See the account [login page](https://urs.earthdata.nasa.gov/) for data access
+///
+/// * General information *
+/// Projection: Geographic
+/// Horizontal Datum: WGS84
+/// Vertical Datum: EGM96 (Earth Gravitational Model 1996)
+/// Vertical Units: Meters
+/// SRTM1: Spatial Resolution: 1 arc-second for global coverage (~30 meters)
+/// SRTM3: Spatial Resolution: 3 arc-seconds for global coverage (~90 meters)
+/// Raster Size:  1 degree tiles
+/// C-band Wavelength: 5.6 cm
+///
 
-// http://dwtkns.com/srtm30m/
-
-const FILEPATH: &str = "/home/aurelien/Documents/data/hgt/N48W002.hgt";
+const DIRPATH: &str = "/home/aurelien/Documents/data/hgt/";
+const FILEPATH: &str = "/home/aurelien/Documents/data/hgt/N49W001.hgt";
 
 fn main() {
-    let result = Tile::from_file(FILEPATH);
-    if let Ok(tile) = result {
-        let Tile {
-            latitude,
-            longitude,
-            resolution,
-            ..
-        } = tile;
-        println!("tile : {}, {}, {:?}", latitude, longitude, resolution);
+    dotenv::from_filename(".env").ok();
+    env_logger::init();
+    let positions = vec![
+        // (lat,lng,x,y,h)
+        (49.99972, -0.99972224, 1, 1, 0, 3602),
+        (49.444443, -0.99972224, 1, 2000, 0, 7202001),
+        (49.02778, -0.99972224, 1, 3500, 118, 12603501),
+        (49.99972, -0.027777791, 3500, 1, 0, 7101),
+        (49.444443, -0.027777791, 3500, 2000, 0, 7205500),
+        (49.02778, -0.027777791, 3500, 3500, 67, 12607000),
+        (49.02778, -0.027777791, 3600, 3600, 67, 12607000),
+    ];
 
-        for x in 3000..3002 {
-            for y in 1800..1802 {
-                let ll =
-                    file_index_to_lat_lng((latitude as f32, longitude as f32), x, y, resolution);
-                println!("lat={}, lng={} -> height={}", ll.0, ll.1, tile.get(x, y));
-            }
-        }
+    let positions2 = positions.clone();
+
+    /*    let result = Tile::from_file(FILEPATH);
+    if let Ok(tile) = result {
+    let Tile {
+    latitude,
+    longitude,
+    resolution,
+    ..
+    } = tile;
+    println!(
+    "tile : {:?},  {}, {}, {:?}",
+    FILEPATH.split("/").last(),
+    latitude,
+    longitude,
+    resolution
+    );
+
+    log::debug!(target: "srtm", "(lat,lng,x,y,h)");
+    positions
+    .into_iter()
+    .for_each(|(_, _, x, y, _, index): (f32, f32, u32, u32, u16, u32)| {
+    let (lat, lng) = resulting_lg((latitude as f32, longitude as f32), x, y, resolution);
+    log::debug!(target: "srtm", "({},{},{},{},{},{})",lat, lng, x, y, tile.get(x, y), index);
+    });
     } else {
-        dbg!("{:?}", result.err());
+    dbg!("{:?}", result.err());
+    }*/
+
+    println!(" --- ");
+
+    positions2
+        .into_iter()
+        .for_each(|(lat, lng, x, y, h, idx): (f32, f32, u32, u32, u16, u32)| {
+            log::debug!(target: "srtm", "expect ({},{},{}, index={})", x, y, h, idx);
+            let res = elevation(DIRPATH, lat, lng);
+            let height = res.unwrap();
+            log::debug!(target: "srtm", "({},{},{},{},{})",lat, lng, x, y, height);
+        });
+}
+
+#[derive(Error, Debug)]
+enum SrtmError {
+    #[error("File size is no STRM tile compatible format")]
+    FileSize,
+}
+
+const SRTM1_FSIZE: u64 = 3601 * 3601 * 2;
+const SRTM3_FSIZE: u64 = 1201 * 1201 * 2;
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub enum Resolution {
+    SRTM1,
+    SRTM3,
+}
+
+impl TryFrom<u64> for Resolution {
+    type Error = SrtmError;
+
+    fn try_from(filesize: u64) -> Result<Self, Self::Error> {
+        match filesize {
+            SRTM1_FSIZE => Ok(Resolution::SRTM1),
+            SRTM3_FSIZE => Ok(Resolution::SRTM3),
+            _ => Err(Self::Error::FileSize),
+        }
+    }
+}
+
+impl Resolution {
+    /// SRTM files are squares.
+    /// The side size depends on the subformat:
+    /// - 3601 values for one earth arc degree (and an overlapped value) with SRTM1
+    /// - 1201 values for one earth arc degree (and an overlapped value) with SRTM3
+    fn side(&self) -> u32 {
+        match &self {
+            Resolution::SRTM1 => 3601,
+            Resolution::SRTM3 => 1201,
+        }
+    }
+    /// Get the number of values available with this format
+    fn count(&self) -> u32 {
+        self.side() * self.side()
+    }
+    /// Get the file size of this resolution
+    fn file_size(&self) -> u32 {
+        self.count() * 2
     }
 }
 
@@ -33,25 +135,34 @@ fn srtm_file_name(lat: f32, lng: f32) -> String {
     let clean = |v: f32| v.floor().abs();
     let ns = if lat >= 0.0 { "N" } else { "S" };
     let ew = if lng >= 0.0 && lng < 180.0 { "E" } else { "W" };
-    format!("{}{:02}{}{:03}", ns, clean(lat), ew, clean(lng)).to_string()
+    format!("{}{:02}{}{:03}.hgt", ns, clean(lat), ew, clean(lng)).to_string()
 }
 
 /// generate srtm pixel coordinates for the given geoposition
 fn srtm_file_coord(lat: f32, lng: f32, resolution: Resolution) -> (u32, u32) {
-    let pixel_index = |v: f32| ((v - v.floor()) * resolution.size() as f32).round() as u32;
-    (pixel_index(lat), pixel_index(lng))
+    let side = resolution.side() - 1;
+    let pixel_index = |v: f32| ((v - v.floor()) * side as f32).round() as u32;
+    (pixel_index(lng), side - pixel_index(lat))
 }
 
-fn elevation<P: AsRef<Path>>(srtm_directory: P, lat: f32, lng: f32) -> i16 {
-    let srtm_file = srtm_file_name(lat, lng); // .push_str(".hgt");
-    srtm_file.push_str(".hgt");
-    let meta = fs::metadata(srtm_directory.as_ref().join(srtm_file))
-        .ok()
-        .and_then(|m: fs::Metadata| Some(m.len()));
-    // let pixel_coord = srtm_file_coord(lat, lng, )
+/// get elevation from lat/lng using srtm files
+fn elevation<P: AsRef<Path>>(srtm_directory: P, lat: f32, lng: f32) -> Result<i16> {
+    let resolution = Resolution::SRTM1;
+    let filename = srtm_file_name(lat, lng);
+    let mut file = File::open(srtm_directory.as_ref().join(filename))?;
+    let resolution = Resolution::try_from(file.metadata()?.len())?;
+    let (x, y) = srtm_file_coord(lat, lng, resolution);
+    let index = x + y * resolution.side();
+    dbg!(index);
+    file.seek(SeekFrom::Start((index * 2) as u64))?;
+    Ok(file.read_i16::<BigEndian>()?)
 }
+
+// fn bounded_elevations(from: (f32, f32), to: (f32, f32)) -> Vec<(f32, f32, i16)> {}
 
 // fn elevations : read a set of point around position
+
+// TESTS
 
 #[test]
 fn test_srtm_file_name() {
@@ -61,22 +172,22 @@ fn test_srtm_file_name() {
     };
 
     // some cases
-    check(49.0, -2.0, "N49W002");
-    check(49.4, -1.3, "N49W002");
-    check(50.9, 1.7, "N50E001");
-    check(-50.9, 1.7, "S51E001");
+    check(49.0, -2.0, "N49W002.hgt");
+    check(49.4, -1.3, "N49W002.hgt");
+    check(50.9, 1.7, "N50E001.hgt");
+    check(-50.9, 1.7, "S51E001.hgt");
     // check l,g around 0,0
-    check(0.0, -0.1, "N00W001");
-    check(-0.0, 0.1, "N00E000");
-    check(0.1, -0.0, "N00E000");
-    check(-0.1, 0.0, "S01E000");
-    check(0.0, -0.0, "N00E000");
-    check(-0.0, 0.0, "N00E000");
+    check(0.0, -0.1, "N00W001.hgt");
+    check(-0.0, 0.1, "N00E000.hgt");
+    check(0.1, -0.0, "N00E000.hgt");
+    check(-0.1, 0.0, "S01E000.hgt");
+    check(0.0, -0.0, "N00E000.hgt");
+    check(-0.0, 0.0, "N00E000.hgt");
     // check around g=180
-    check(45.0, 179.0, "N45E179");
-    check(45.0, 180.0, "N45W180");
-    check(45.0, 179.9, "N45E179");
-    check(45.0, -180.0, "N45W180");
+    check(45.0, 179.0, "N45E179.hgt");
+    check(45.0, 180.0, "N45W180.hgt");
+    check(45.0, 179.9, "N45E179.hgt");
+    check(45.0, -180.0, "N45W180.hgt");
     // unsupported cases
     // check(0.0, -181.0, "N00E179");
     // check(0.0, 181.0, "N45W179");
@@ -94,39 +205,33 @@ fn test_srtm_file_coord() {
     check(48.833103, -1.5001389, (3000, 1800));
 }
 
-fn file_index_to_lat_lng(base: (f32, f32), x: u32, y: u32, res: Resolution) -> (f32, f32) {
-    let adjust = |v| v / res.size() as f32;
-    println!(
-        "(base.0 + {}, base.1 + {})",
-        adjust(x as f32),
-        adjust(y as f32)
-    );
-    println!("x = {}, y = {}", x, y);
-    (base.0 + adjust(x as f32), base.1 + adjust(y as f32))
+#[test]
+fn test_elevation() {
+    let h = elevation(DIRPATH, 48.833103, -1.5001389);
+    let h = h.unwrap();
+    println!("ELEVATION IS {}", h); //160
+    assert_eq!(h, 160);
+}
+
+fn resulting_lg(
+    base: (f32, f32),
+    offset_x_lng: u32,
+    offset_y_lat: u32,
+    res: Resolution,
+) -> (f32, f32) {
+    let adjust = |v| v / (res.side() - 1) as f32;
+    (
+        base.0 + 1.0 - adjust(offset_y_lat as f32),
+        base.1 + adjust(offset_x_lng as f32),
+    )
 }
 
 use std::fs::File;
 
 use byteorder::{BigEndian, ReadBytesExt};
-use std::io;
+use std::io::{self, Seek, SeekFrom};
 use std::io::{BufReader, Read};
 use std::path::Path;
-
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-pub enum Resolution {
-    SRTM1,
-    SRTM3,
-}
-
-impl Resolution {
-    fn size(&self) -> u16 {
-        if self == &Resolution::SRTM1 {
-            3601
-        } else {
-            1201
-        }
-    }
-}
 
 #[derive(Debug)]
 pub struct Tile {
@@ -175,12 +280,13 @@ impl Tile {
     }
 
     pub fn get(&self, x: u32, y: u32) -> i16 {
+        log::warn!("lib found index = {}", self.idx(x, y));
         self.data[self.idx(x, y)]
     }
 
     fn idx(&self, x: u32, y: u32) -> usize {
         assert!(x < self.extent() && y < self.extent());
-        (y * self.extent() + x) as usize
+        (y * (self.extent()) + x) as usize
     }
 }
 
